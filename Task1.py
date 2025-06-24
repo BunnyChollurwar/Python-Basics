aa3b30942e64fcf56be6013b81ce01c9e6c98897
BunnyChollurwar <bunnyharishchollurwar@gmail.com>
2025-06-24 09:42:42 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,13 @@
+#---> TASK1:
+#Taking two input numbers from the user:
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+
+#Performing Different Operations:
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+
+#Printing the output:
+print("Addition:",a,"\nSubtraction:",b,"\nMultiplication:",c,"\nDivision:",d)
---
+++ Task2.py
@@ -0,0 +1,10 @@
+#--->>> TASK2:
+#CREATING A PERSONALISED GREETING:
+first_name=input('Enter your first name:')
+last_name=input('Enter your last name:')
+
+#Concatenating first and last name:
+full_name= first_name + " " + last_name
+
+#Printing the Message:
+print("Hello, " + full_name +"! Welcome to the Python World ")
